0068bffb940bf29b20a54fb6414bce4e086dae77
CHICHCPU <nguyenquivinhquang@gmail.com>
2022-05-15 10:19:33 +0700

    Update semester billing

---
+++ src/main/java/hcmiucvip/solutionforsavingstudentrecords/SemesterBillingView.java
@@ -0,0 +1,74 @@
+package hcmiucvip.solutionforsavingstudentrecords;
+
+public class SemesterBillingView {
+
+}
+
+class SemesterBilling {
+    Integer year, semester, credits;
+    Double pricePerCredit, insurancePrice, total;
+    Double moneyReceived;
+
+    public SemesterBilling() {
+
+    }
+
+    public SemesterBilling(Integer year, Integer semester, Integer credits,
+                           Double pricePerCredit, Double insurancePrice, Double moneyReceived) {
+        this.year = year;
+        this.semester = semester;
+        this.credits = credits;
+        this.pricePerCredit = pricePerCredit;
+        this.insurancePrice = insurancePrice;
+        this.moneyReceived = moneyReceived;
+
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public void setSemester(Integer semester) {
+        this.semester = semester;
+    }
+
+    public void setPricePerCredit(Double pricePerCredit) {
+        this.pricePerCredit = pricePerCredit;
+    }
+
+    public void setInsurancePrice(Double insurancePrice) {
+        this.insurancePrice = insurancePrice;
+    }
+
+    public Integer getSemester() {
+        return semester;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public Double getPricePerCredit() {
+        return pricePerCredit;
+    }
+
+    public Double getInsurancePrice() {
+        return insurancePrice;
+    }
+
+    public Integer getCredits() {
+        return credits;
+    }
+
+    public void setCredits(Integer credits) {
+        this.credits = credits;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Double getTotal() {
+        return pricePerCredit * credits + insurancePrice;
+    }
+}
